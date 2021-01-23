f992389990d07d776f327584884e80d54cae5bdd
jenny-s51 <jshandel12@gmail.com>
2021-01-22 19:12:50 -0500

    reduce

--- array-methods/src/App.js
+++ array-methods/src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 
 function App() {
   const bopsFromThe80s = [
-    { title: "Jump", artist: "Van Halen" },
-    { title: "Ghostbusters Theme", artist: "Ray Parker Jr." },
-    { title: "Girls Just Want to Have Fun", artist: "Cyndi Lauper" },
-    { title: "I Melt with You", artist: "Modern English" },
-    { title: "Should I Stay or Should I Go", artist: "The Clash" },
-    { title: "Africa", artist: "TOTO" },
-    { title: "Don't Dream It's Over", artist: "Crowded House" },
-    { title: "Jessie's Girl", artist: "Rick Springfield" },
+    { title: "Jump", artist: "Van Halen", playlistItem: 6 },
+    { title: "Ghostbusters Theme", artist: "Ray Parker Jr.", playlistItem: 4 },
+    { title: "Girls Just Want to Have Fun", artist: "Cyndi Lauper", playlistItem: 5 },
+    { title: "I Melt with You", artist: "Modern English", playlistItem: 2 },
+    { title: "Should I Stay or Should I Go", artist: "The Clash", playlistItem: 3 },
+    { title: "Africa", artist: "TOTO", playlistItem: 1 },
+    { title: "Don't Dream It's Over", artist: "Crowded House", playlistItem: 8 },
+    { title: "Jessie's Girl", artist: "Rick Springfield", playlistItem: 7 },
   ];
 
   // filter method - doesn't change the underlying array!
@@ -29,19 +29,24 @@ function App() {
   // forEach - when we want to do something to all of the items
   bopsFromThe80s.forEach((bop) => console.log(bop.title));
 
-  // some method: checks condition and returns TRUE of some items satisfy the condition
+  // some method - checks condition and returns TRUE of some items satisfy the condition
   const Africa = bopsFromThe80s.some((bop) => bop.title === "Africa");
   if (Africa) {
-    // important
+    // 🎶 there's nothing that 100 men or more could ever do 🎶
     console.log("Bless the rains")
   }
 
-  // every method: like the some method, except it's for all of them
+  // every method - like the some method, except it's for all of them
   const hasApostrophe = bopsFromThe80s.every((bop) => bop.title.includes("'"))
   // false - not all songs have apostrophes :'(
   console.log(hasApostrophe)
 
 
+  // reduce method - runs a function on every single item of the array
+  const addingNumbers = bopsFromThe80s.reduce((currentTotal, bop) => {
+   return bop.playlistItem + currentTotal
+  }, 0)
+  console.log(addingNumbers)
 
   return (
     <div className="App">
