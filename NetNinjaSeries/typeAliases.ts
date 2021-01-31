69e413523f60ead570f6d8c671cb91035a5e6105
jenny-s51 <jshandel12@gmail.com>
2021-01-31 16:58:01 -0500

    netninja aliases

---
+++ NetNinjaSeries/typeAliases.ts
@@ -0,0 +1,11 @@
+type StringOrNum = string | number;
+type ObjWithName = { name: string, uid: StringOrNum}
+
+const logDetails = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`)
+}
+
+// we don't have to rewrite anything now
+const greet = (user: ObjWithName) => {
+    console.log(`${user.name} says hello!`)
+}
